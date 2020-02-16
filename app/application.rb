77b8ac8bd3312e99659531005b77d6fffe73abea
Anthony Mai <anthony.mai530@gmail.com>
2020-02-16 04:36:36 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class application
+  @@items = []
+
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+
+      item = @@items.collect {|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not Found"
+      resp.status = 404
+    end
+    resp.finsh
+
+  end
+
+end
